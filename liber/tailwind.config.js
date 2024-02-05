984cafa85b1d983c67227676c51b404b2b648628
ea57 <ea57@alu.ua.es>
2024-02-05 20:04:50 +0100

    haciendo responsive el menu

--- liber/tailwind.config.js
+++ liber/tailwind.config.js
@@ -1,12 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
@@ -17,5 +18,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [],
 };
