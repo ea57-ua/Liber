ba7c726edbfe68766770fb28faf02a60dd536bab
ea57 <ea57@alu.ua.es>
2024-04-05 20:34:17 +0200

    directors details page

--- liber/app/Http/Controllers/DirectorController.php
+++ liber/app/Http/Controllers/DirectorController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Director;
 use Illuminate\Http\Request;
 
 class DirectorController extends Controller
 {
     public function showDirectorInfo($id)
     {
-        return view('directors.directorDetails');
+        $director = Director::findOrfail($id);
+        $movies = $director->movies()->get();
+        $averageRatingGlobal = $movies->avg(function ($movie) {
+            return $movie->getAverageRatingAttribute();
+        });
+        $averageRatingCritics = $movies->avg(function ($movie) {
+            return $movie->getCriticAverageAttribute();
+        });
+        return view('directors.directorDetails',
+            ['director' => $director,
+                'movies' => $movies,
+                'averageRatingGlobal' => $averageRatingGlobal,
+                'averageRatingCritics' => $averageRatingCritics]);
     }
 }
--- liber/resources/views/directors/directorDetails.blade.php
+++ liber/resources/views/directors/directorDetails.blade.php
@@ -1,5 +1,64 @@
 @extends('layouts.master')
-@section('title', 'Liber - Director profile')
+@section('title', 'Liber - Director Information')
 @section('content')
-    <h1>director</h1>
+    <div class="container celebrity-info-container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-4">
+                <a href="{{ asset($director->photo) }}"
+                   data-gallery="portfolio-gallery-app" class="glightbox">
+                    <img id="movie-poster" src="{{ asset($director->photo) }}"
+                         class="movie-poster" alt="Director photo">
+                </a>
+            </div>
+
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-8">
+                <div class="celebrity-info">
+                    <h1>{{$director->name}}</h1>
+                    <div class="row mb-1">
+                        <div class="col-sm-12 col-lg-12 col-xl-12">
+                            <div class="celebrity-description">
+                                <p>{{$director->description}}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="movies-details-separator"></div>
+
+
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 d-flex justify-content-center">
+                            <div class="movie-details-text d-flex flex-column align-items-center">
+                                <p class="movie-ratings-text">{{$averageRatingCritics}}/10</p>
+                                <p class="movie-rating-legend">Critics average</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 d-flex justify-content-center">
+                            <div class="movie-details-text d-flex flex-column align-items-center">
+                                <p class="movie-ratings-text">{{$averageRatingGlobal}}/10</p>
+                                <p class="movie-rating-legend">Global average</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <section id="movies" class="movies">
+            <div data-aos="fade-up">
+                <div class="row gy-4">
+                    @foreach($movies as $movie)
+                        <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
+                            <a href="{{ route('movies.details', ['id' => $movie->id]) }}">
+                                <div class="movieCard">
+                                    <img src="{{$movie->posterURL}}" class="img-fluid" alt="">
+                                    <h4>{{$movie->title}}</h4>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </div>
 @endsection
