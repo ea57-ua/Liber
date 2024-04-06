ee6b72d437cb4053095a1e998d4cd5cc95d8ff7b
ea57 <ea57@alu.ua.es>
2024-04-06 09:59:21 +0200

    quitar middleware auth para home

--- liber/app/Http/Controllers/HomeController.php
+++ liber/app/Http/Controllers/HomeController.php
@@ -8,16 +8,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         $popularMovies = Movie::all()
