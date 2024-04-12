3b2f6b1a70f24d32e47f3de20a39dab63b160e5d
ea57 <ea57@alu.ua.es>
2024-04-12 16:59:42 +0200

    empezando reportes en admin

--- liber/app/Http/Controllers/Admin/ReportsController.php
+++ liber/app/Http/Controllers/Admin/ReportsController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
     public function showReportsAdminPanel(){
         $admin = auth()->user();
+        $reports = Report::paginate(10);
         return view('admin.reports.reportsList',
-            ['admin' => $admin]);
+            ['admin' => $admin,
+                'reports' => $reports]);
     }
 }
--- liber/resources/views/admin/reports/reportsList.blade.php
+++ liber/resources/views/admin/reports/reportsList.blade.php
@@ -12,5 +12,42 @@
         </nav>
     </div>
 
+    <section>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Reports list</h5>
+
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">Reporter</th>
+                        <th scope="col">Reason</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Reported post</th>
+                        <th scope="col">Actions</th>
+                    </thead>
+
+                    <tbody>
+                    @foreach($reports as $report)
+                        <tr>
+                            <td><a href="{{route('users.publicProfile', $movie->id)}}">{{$report->user->name}}</a></td>
+                            <td>{{ \Illuminate\Support\Str::limit($movie->synopsis, 100, '...') }}</td>
+                            <td>{{ \Carbon\Carbon::parse($movie->releaseDate)->format('Y') }}</td>
+                            <td>
+
+                            </td>
+                        </tr>
+
+                    @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+
+        <div class="justify-content-center">
+            {{ $reports->links() }}
+        </div>
+    </section>
 
 @endsection
