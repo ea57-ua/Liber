2fa5aff141cbdee8d8842c3bf7bf9cdb09ff65ec
ea57 <ea57@alu.ua.es>
2024-05-06 20:56:38 +0200

    seeders de reportes de posts

--- liber/database/seeders/PostReportsSeeder.php
+++ liber/database/seeders/PostReportsSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Report;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class PostReportsSeeder extends Seeder
 {
@@ -12,6 +16,41 @@ class PostReportsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        $categories = ['Spam',
+                        'Harassment',
+                        'Inappropriate Content',
+                        'Privacy Violation',
+                        'Bullying',
+                        'Other'];
+
+        $users = [
+            User::where('email', 'johndoe@gmail.com')->first(),
+            User::where('email', 'stuart@gmail.com')->first(),
+            User::where('email', 'juan@gmail.com')->first(),
+            User::where('email', 'vlad@gmail.com')->first(),
+            User::where('email', 'jessica@gmail.com')->first(),
+        ];
+
+        foreach ($users as $user) {
+            $userPosts = Post::where('user_id', $user->id)->get();
+
+            $numReports = rand(1, 3);
+
+            for ($i = 0; $i < $numReports; $i++) {
+                $otherUser = $users[array_rand($users)];
+                while ($otherUser->id === $user->id) {
+                    $otherUser = $users[array_rand($users)];
+                }
+                $otherUserPost = Post::where('user_id', $otherUser->id)->inRandomOrder()->first();
+
+                $report = new Report();
+                $report->reason = $faker->sentence;
+                $report->category = $categories[array_rand($categories)];
+                $report->post_id = $otherUserPost->id;
+                $report->user_id = $user->id;
+                $report->save();
+            }
+        }
     }
 }
