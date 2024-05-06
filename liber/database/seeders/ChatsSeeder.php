cf114b2bf7e3bf0ef525625b563f185df0e9063f
ea57 <ea57@alu.ua.es>
2024-05-06 21:09:47 +0200

    seeders de mensajess

--- liber/database/seeders/ChatsSeeder.php
+++ liber/database/seeders/ChatsSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\ChMessage;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class ChatsSeeder extends Seeder
 {
@@ -12,6 +15,32 @@ class ChatsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        $user = User::where('email', 'user@liber.com')->first();
+        $critic = User::where('email', 'critic@liber.com')->first();
+        $admin = User::where('email', 'admin@liber.com')->first();
+
+        $conversations = [
+            ['from' => $user, 'to' => $critic],
+            ['from' => $user, 'to' => $admin],
+        ];
+
+        foreach ($conversations as $conversation) {
+            $numMessages = rand(15, 50);
+
+            for ($i = 0; $i < $numMessages; $i++) {
+                $message = new ChMessage();
+                if ($i % 2 == 0) {
+                    $message->from_id = $conversation['from']->id;
+                    $message->to_id = $conversation['to']->id;
+                } else {
+                    $message->from_id = $conversation['to']->id;
+                    $message->to_id = $conversation['from']->id;
+                }
+                $message->body = $faker->sentence;
+                $message->save();
+            }
+        }
     }
 }
